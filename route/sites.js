7150d9ecea1301efaffcc16d1780d00dfcc21b96
Kerem Yavuz <=>
2017-07-05 15:42:18 +0300

    Site gösterim dosyaları, route işlemleri

---
+++ route/sites.js
@@ -0,0 +1,64 @@
+var path = require("path");
+var fs = require("fs");
+var root = path.resolve('views');
+module.exports = function (app) {
+
+
+    // Queryden gelen parametreleri alıyoruz
+    function getParams(req) {
+        var param = req.params;
+
+        // Bizim belirlediğimiz parametreler
+        return { site: param.sitename, page: param.pagename };
+    }
+
+    // Gelen query bilgisi, yani sitename eğer bazı kelimeleri içeriyorsa NEXT() diyip devam edeceğiz, değilse kontrole sokacağız
+    function isFilter(req, action) {
+        var param = getParams(req);
+        if (/assets/i.test(param.site)) {
+            action(param);
+            return false;
+        }
+        return true;
+    }
+
+    function getURL(req, next) {
+        var param = getParams(req);
+        if (param.page)
+            param.page = root + '/' + param.page;
+        else if (!param.page)
+            param.page = root + '/index.jade';
+        else
+            param.page = root + '/error.jade';
+
+        return param;
+    }
+
+
+    // Çağırılan URL bilgisinin tetikleneceği fonksiyon
+    function callback(req, res, next) {
+
+        // Eğer özel dosya yolları varsa
+        var result = isFilter(req, function (param) {
+
+            var n = path.resolve(param.site + '/' + param.page);
+
+            // Gelen dosyanın yolunu kontrol ederek. Aradığı dosyayı veya sayfayı bulamadığında özel bir uyarı sayfası gösterelim
+            if (!fs.existsSync(n)) {
+                res.render('nofile');
+            }
+            else
+                res.sendFile(n);
+        });
+
+        if (result) {
+            var data = getURL(req);
+            res.render(data.page);
+        }
+    }
+
+    app.get('/:sitename/:pagename', callback);
+
+    app.get('/:sitename', callback);
+
+}
