5501e1333f27c42e69d6218e6b8a90f4bb0edfae
Kerem Yavuz <=>
2017-07-05 15:58:42 +0300

    port update

---
+++ index_dev.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var parser = require("body-parser");
+var fs = require('fs');
+var home = require("./route/home")(app);
+var sites = require("./route/sites")(app);
+var path = require("path");
+// Middleware
+
+app.use('/assets', express.static('assets'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade')
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+
+app.listen(12345);
