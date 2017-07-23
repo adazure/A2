650158b89243d136844bc42244e8a4da6fe51684
Keremix © <aske2005@hotmail.com>
2017-07-23 09:15:34 +0300

    Güncelleme

--- assets/js/common.js
+++ assets/js/common.js
@@ -135,148 +135,270 @@ function initMap() {
     contactForm[0].addEventListener('submit', postEvent);
 
 })();
-(function(config) {
+var fsgallery = (function(config) {
 
+    var currentIndex = -1;
+    var currentItem = null;
+    var zIndex = 100;
+    var fsgStatus = false;
+    var elements = {};
 
-    config.containers = document.getElementsByClassName('slide-container');
+    function check(url) {
+        var http = new XMLHttpRequest();
+        http.open('HEAD', url, false);
+        http.send();
+        return http.status != 404;
+    }
 
-    function template(data) {
-        var self = this;
-        self.status = true;
-        self.container = data;
-        self.template = data.children[0];
-        self.items = self.template.children;
-        self.options = { startIndex: 0, timer: 3000 };
-        self.position = { left: false, right: false };
-        self.dotContainer = document.createElement('div');
-        self.dotContainer.className = 'slide-container-dots';
-        self.container.appendChild(self.dotContainer);
+    function create() {
+        elements.shadow = document.createElement('div');
+        elements.prev = document.createElement('div');
+        elements.next = document.createElement('div');
+        elements.shadow.id = 'fsg-shadow';
+        elements.prev.id = 'fsg-prev';
+        elements.prev.className = elements.next.className = 'fsg-arrow';
+        elements.next.id = 'fsg-next';
 
-        self.eventnames = {
-            click: ['click', 'touchend'],
-            mousedown: ['mousedown', 'touchstart'],
-            mouseup: ['mouseup', 'touchend'],
-            mousemove: ['mousemove', 'touchmove']
+        fsgStatus = true;
+        appendItem(document.body, elements.shadow, elements.next, elements.prev);
+    }
+
+    function show(index) {
+
+        if (!fsgStatus) create();
+
+        remove();
+
+        var container = document.createElement('div');
+        container.className = 'fsg-item';
+        container.style.zIndex = zIndex;
+        if (check(config.images[index])) {
+            var image = new Image();
+            image.src = config.images[index].getAttribute('data-fullscreen');
+            container.appendChild(image);
+            zIndex++;
+            currentIndex = index;
+            currentItem = container;
+            document.body.appendChild(container);
+            currentItem.addEventListener('click', hide, false);
+            return container;
         }
-
-        self.bind = function(obj, name, action) {
-
-            function sets(a) {
-                if (obj.addEventListener)
-                    obj.addEventListener(a, action);
-                else
-                    obj.attachEvent('on' + a, action);
-            }
-            for (var i = 0; i < self.eventnames[name].length; i++)
-                sets(self.eventnames[name][i]);
-        }
-
-        self.add = function(n, i) {
-            var dot = document.createElement('span');
-            self.bind(dot, 'click', function() {
-                self.options.startIndex = i;
-                self.selectedItem();
-            });
-            self.dotContainer.appendChild(dot);
-        }
-
-        self.selectedItem = function() {
-            for (var i = 0, u = self.dotContainer.children; i < u.length; i++) {
-                u[i].className = '';
-            }
-
-            u[self.options.startIndex].className = 'active';
-            self.template.style.left = -(self.options.startIndex * self.container.clientWidth) + 'px';
-            self.watch();
-        }
-
-
-        for (var i = 0; i < self.items.length; i++) {
-            self.add(self.items[i], i);
-        }
-
-        self.getPosition = function(e) {
-            var result = { pageX: 0, pageY: 0 };
-            result.pageX = e.touches ? e.touches[0].pageX : e.pageX;
-            result.pageY = e.touches ? e.touches[0].pageY : e.pageY;
-            return result;
-        }
-
-        self.bind(self.container, 'mousedown', function(e) {
-            var x = self.getPosition(e).pageX;
-            self.position = { leftX: x, x: x - self.template.offsetLeft };
-            self.template.classList.add('handle');
-            e.preventDefault();
-            self.status = false;
-            return;
-        });
-
-        self.bind(self.container, 'mousemove', function(e) {
-            e.preventDefault();
-            if (!self.status) {
-                var x = self.getPosition(e).pageX;
-                self.position.left = self.position.leftX > x;
-                self.position.right = self.position.leftX < x;
-                var min = (self.template.children.length * self.container.offsetWidth) - self.container.offsetWidth;
-                self.position.current = x - self.container.offsetLeft - self.position.x;
-                if (self.position.current > 0)
-                    self.position.current = 0;
-                else if (self.position.current < -min)
-                    self.position.current = -min;
-                self.template.style.left = self.position.current + 'px';
-            }
-            return false;
-        });
-        self.bind(window, 'mouseup', function() {
-            self.template.classList.remove('handle');
-            if (self.position.left) {
-                self.options.startIndex++;
-                self.checkPosition();
-                self.selectedItem();
-            } else if (self.position.right) {
-                self.options.startIndex--;
-                self.checkPosition();
-                self.selectedItem();
-            }
-
-            self.status = true;
-            self.watch();
-
-            self.position.left = self.position.right = false;
-        });
-
-        self.checkPosition = function() {
-            if (self.options.startIndex > self.items.length - 1)
-                self.options.startIndex = self.items.length - 1;
-
-            if (self.options.startIndex < 0)
-                self.options.startIndex = 0;
-        }
-        var time = 0;
-        self.watch = function() {
-            clearTimeout(time);
-            time = setTimeout(function() {
-                if (self.status) {
-
-                    if (self.options.startIndex < self.items.length - 1) {
-                        self.options.startIndex++;
-                    } else
-                        self.options.startIndex = 0;
-
-                    self.selectedItem(self.options.startIndex);
-
-                }
-            }, self.options.timer);
-        }
-        self.selectedItem(self.options.startIndex);
 
     }
 
-    (function(a) {
-        for (var n = 0; n < a.length; n++) {
-            var t = new template(a[n]);
+    function removeItem() {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i])
+                arguments[i].parentNode.removeChild(arguments[i])
         }
-    })(config.containers);
+    }
+
+    function appendItem(root) {
+        var k = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0; i < k.length; i++) {
+            root.appendChild(k[i]);
+        }
+    }
+
+    function hide() {
+        removeItem(elements.shadow, elements.prev, elements.next);
+        remove(currentIndex, currentItem);
+        currentIndex = -1;
+        currentItem = null;
+        fsgStatus = false;
+    }
+
+    function remove(i, z) {
+        z.className = 'fsg-item fsg-removed';
+        setTimeout(function() { z.parentNode.removeChild(z); }, config.removeTimer);
+    }
+
+    function start(args) {
+        config.images = document.querySelectorAll(args.data);
+        if (config.images) {
+            for (var a = 0, b = config.images; a < b.length; a++) {
+                b[a].index = a;
+                b[a].addEventListener('click', function() {
+                    show(this.index);
+                }, false);
+            }
+        }
+        config.removeTimer = args.removeTimer || 200;
+    }
+
+    function next() {
+        if (currentIndex < config.images.length) {
+            currentIndex++;
+            show(currentIndex);
+        }
+
+        showHideArrow();
+    }
+
+    function prev() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            show(currentIndex);
+        }
+        showHideArrow();
+    }
+
+    function showHideArrow() {
+        if (elements.prev)
+            elements.prev.style.display = currentIndex <= 0 ? 'none' : 'block';
+
+        if (elements.next)
+            elements.next.style.display = currentIndex >= config.images.length - 1
+    }
+
+
+    return { init: start, next: next, prev: prev, hide: hide, show: show };
+})({});
+(function(config) {
+
+    window.addEventListener('load', function() {
+
+        config.containers = document.getElementsByClassName('slide-container');
+
+        function template(data) {
+            var self = this;
+            self.status = true;
+            self.container = data;
+            self.template = data.children[0];
+            self.items = self.template.children;
+            self.options = { startIndex: 0, timer: 3000 };
+            self.position = { left: false, right: false };
+            self.dotContainer = document.createElement('div');
+            self.dotContainer.className = 'slide-container-dots';
+            self.container.appendChild(self.dotContainer);
+
+            self.eventnames = {
+                click: ['click', 'touchend'],
+                mousedown: ['mousedown', 'touchstart'],
+                mouseup: ['mouseup', 'touchend'],
+                mousemove: ['mousemove', 'touchmove']
+            }
+
+            self.bind = function(obj, name, action) {
+
+                function sets(a) {
+                    if (obj.addEventListener)
+                        obj.addEventListener(a, action);
+                    else
+                        obj.attachEvent('on' + a, action);
+                }
+                for (var i = 0; i < self.eventnames[name].length; i++)
+                    sets(self.eventnames[name][i]);
+            }
+
+            self.add = function(n, i) {
+                var dot = document.createElement('span');
+                self.bind(dot, 'click', function() {
+                    self.options.startIndex = i;
+                    self.selectedItem();
+                });
+                self.dotContainer.appendChild(dot);
+            }
+
+            self.selectedItem = function() {
+                for (var i = 0, u = self.dotContainer.children; i < u.length; i++) {
+                    u[i].className = '';
+                }
+
+                u[self.options.startIndex].className = 'active';
+                self.template.style.left = -(self.options.startIndex * self.container.clientWidth) + 'px';
+                self.watch();
+            }
+
+
+            for (var i = 0; i < self.items.length; i++) {
+                self.add(self.items[i], i);
+            }
+
+            self.getPosition = function(e) {
+                var result = { pageX: 0, pageY: 0 };
+                result.pageX = e.touches ? e.touches[0].pageX : e.pageX;
+                result.pageY = e.touches ? e.touches[0].pageY : e.pageY;
+                return result;
+            }
+
+            self.bind(self.container, 'mousedown', function(e) {
+                var x = self.getPosition(e).pageX;
+                self.position = { leftX: x, x: x - self.template.offsetLeft };
+                self.template.classList.add('handle');
+                e.preventDefault();
+                self.status = false;
+                return;
+            });
+
+            self.bind(self.container, 'mousemove', function(e) {
+                e.preventDefault();
+                if (!self.status) {
+                    var x = self.getPosition(e).pageX;
+                    self.position.left = self.position.leftX > x;
+                    self.position.right = self.position.leftX < x;
+                    var min = (self.template.children.length * self.container.offsetWidth) - self.container.offsetWidth;
+                    self.position.current = x - self.container.offsetLeft - self.position.x;
+                    if (self.position.current > 0)
+                        self.position.current = 0;
+                    else if (self.position.current < -min)
+                        self.position.current = -min;
+                    self.template.style.left = self.position.current + 'px';
+                }
+                return false;
+            });
+            self.bind(window, 'mouseup', function() {
+                self.template.classList.remove('handle');
+                if (self.position.left) {
+                    self.options.startIndex++;
+                    self.checkPosition();
+                    self.selectedItem();
+                } else if (self.position.right) {
+                    self.options.startIndex--;
+                    self.checkPosition();
+                    self.selectedItem();
+                }
+
+                self.status = true;
+                self.watch();
+
+                self.position.left = self.position.right = false;
+            });
+
+            self.checkPosition = function() {
+                if (self.options.startIndex > self.items.length - 1)
+                    self.options.startIndex = self.items.length - 1;
+
+                if (self.options.startIndex < 0)
+                    self.options.startIndex = 0;
+            }
+            var time = 0;
+            self.watch = function() {
+                clearTimeout(time);
+                time = setTimeout(function() {
+                    if (self.status) {
+
+                        if (self.options.startIndex < self.items.length - 1) {
+                            self.options.startIndex++;
+                        } else
+                            self.options.startIndex = 0;
+
+                        self.selectedItem(self.options.startIndex);
+
+                    }
+                }, self.options.timer);
+            }
+            self.selectedItem(self.options.startIndex);
+
+        }
+
+        (function(a) {
+            for (var n = 0; n < a.length; n++) {
+                var t = new template(a[n]);
+            }
+        })(config.containers);
+
+    });
 })({});
 /*
     (-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)(-)
---
+++ devtools/scripts/fullscreen.gallery.js
@@ -0,0 +1,119 @@
+var fsgallery = (function(config) {
+
+    var currentIndex = -1;
+    var currentItem = null;
+    var zIndex = 100;
+    var fsgStatus = false;
+    var elements = {};
+
+    function check(url) {
+        var http = new XMLHttpRequest();
+        http.open('HEAD', url, false);
+        http.send();
+        return http.status != 404;
+    }
+
+    function create() {
+        elements.shadow = document.createElement('div');
+        elements.prev = document.createElement('div');
+        elements.next = document.createElement('div');
+        elements.shadow.id = 'fsg-shadow';
+        elements.prev.id = 'fsg-prev';
+        elements.prev.className = elements.next.className = 'fsg-arrow';
+        elements.next.id = 'fsg-next';
+
+        fsgStatus = true;
+        appendItem(document.body, elements.shadow, elements.next, elements.prev);
+    }
+
+    function show(index) {
+
+        if (!fsgStatus) create();
+
+        remove();
+
+        var container = document.createElement('div');
+        container.className = 'fsg-item';
+        container.style.zIndex = zIndex;
+        if (check(config.images[index])) {
+            var image = new Image();
+            image.src = config.images[index].getAttribute('data-fullscreen');
+            container.appendChild(image);
+            zIndex++;
+            currentIndex = index;
+            currentItem = container;
+            document.body.appendChild(container);
+            currentItem.addEventListener('click', hide, false);
+            return container;
+        }
+
+    }
+
+    function removeItem() {
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i])
+                arguments[i].parentNode.removeChild(arguments[i])
+        }
+    }
+
+    function appendItem(root) {
+        var k = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0; i < k.length; i++) {
+            root.appendChild(k[i]);
+        }
+    }
+
+    function hide() {
+        removeItem(elements.shadow, elements.prev, elements.next);
+        remove(currentIndex, currentItem);
+        currentIndex = -1;
+        currentItem = null;
+        fsgStatus = false;
+    }
+
+    function remove(i, z) {
+        z.className = 'fsg-item fsg-removed';
+        setTimeout(function() { z.parentNode.removeChild(z); }, config.removeTimer);
+    }
+
+    function start(args) {
+        config.images = document.querySelectorAll(args.data);
+        if (config.images) {
+            for (var a = 0, b = config.images; a < b.length; a++) {
+                b[a].index = a;
+                b[a].addEventListener('click', function() {
+                    show(this.index);
+                }, false);
+            }
+        }
+        config.removeTimer = args.removeTimer || 200;
+    }
+
+    function next() {
+        if (currentIndex < config.images.length) {
+            currentIndex++;
+            show(currentIndex);
+        }
+
+        showHideArrow();
+    }
+
+    function prev() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            show(currentIndex);
+        }
+        showHideArrow();
+    }
+
+    function showHideArrow() {
+        if (elements.prev)
+            elements.prev.style.display = currentIndex <= 0 ? 'none' : 'block';
+
+        if (elements.next)
+            elements.next.style.display = currentIndex >= config.images.length - 1
+    }
+
+
+    return { init: start, next: next, prev: prev, hide: hide, show: show };
+})({});
